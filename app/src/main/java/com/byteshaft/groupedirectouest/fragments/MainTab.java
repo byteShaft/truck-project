b9c09b855d6a20b66c03d4e417f2aa700c4e9c00
s9iper1 <s9iper1@gmail.com>
2015-09-18 17:00:09 +0500

    add swipe tabs , splash screen

---
+++ app/src/main/java/com/byteshaft/groupedirectouest/fragments/MainTab.java
@@ -0,0 +1,27 @@
+package com.byteshaft.groupedirectouest.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.byteshaft.groupedirectouest.R;
+
+
+public class MainTab extends Fragment {
+
+    public static final String ARG_OBJECT = "object";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        // The last two arguments ensure LayoutParams are inflated
+        // properly.
+        View rootView = inflater.inflate(
+                R.layout.mainview, container, false);
+        return rootView;
+    }
+
+
+}
---
+++ app/src/main/java/com/byteshaft/groupedirectouest/fragments/WebViewFragment.java
@@ -0,0 +1,26 @@
+package com.byteshaft.groupedirectouest.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.byteshaft.groupedirectouest.R;
+
+
+public class WebViewFragment extends Fragment {
+
+    public static final String ARG_OBJECT = "object";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        // The last two arguments ensure LayoutParams are inflated
+        // properly.
+        View rootView = inflater.inflate(
+                R.layout.webviewfragment, container, false);
+        return rootView;
+    }
+
+}
