6603f161ad056f059dfc7e9050b0dff43db535c2
s9iper1 <s9iper1@gmail.com>
2015-09-21 12:54:51 +0500

    git add app/src/main/res/drawable/about.png

--- app/src/main/java/com/byteshaft/groupedirectouest/fragments/MainTab.java
+++ app/src/main/java/com/byteshaft/groupedirectouest/fragments/MainTab.java
@@ -5,23 +5,38 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import com.byteshaft.groupedirectouest.R;
 
 
-public class MainTab extends Fragment {
+public class MainTab extends Fragment implements View.OnClickListener {
 
     public static final String ARG_OBJECT = "object";
+    private View mBaseView;
+    private RelativeLayout mRelativeLayout;
+    private Button mButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        // The last two arguments ensure LayoutParams are inflated
-        // properly.
-        View rootView = inflater.inflate(
-                R.layout.mainview, container, false);
-        return rootView;
+        mBaseView= inflater.inflate(R.layout.mainview, container, false);
+        mRelativeLayout = (RelativeLayout) mBaseView.findViewById(R.id.tuckRelativeLayout);
+        mRelativeLayout.setBackgroundDrawable(getResources().getDrawable(R.drawable.gdo));
+        mButton = (Button) mBaseView.findViewById(R.id.button);
+        mButton.setOnClickListener(this);
+        return mBaseView;
     }
 
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+
+                break;
+        }
+
+    }
 }
