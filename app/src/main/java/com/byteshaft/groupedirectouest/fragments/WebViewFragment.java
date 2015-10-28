c5009ff5b4d0c1b6f94960c1a7f7e29aae70d148
s9iper1 <s9iper1@gmail.com>
2015-10-28 12:07:22 +0500

    add webview

--- app/src/main/java/com/byteshaft/groupedirectouest/fragments/WebViewFragment.java
+++ app/src/main/java/com/byteshaft/groupedirectouest/fragments/WebViewFragment.java
@@ -6,20 +6,38 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.byteshaft.groupedirectouest.R;
 
 
 public class WebViewFragment extends Fragment {
 
+    private View mBaseView;
+    private WebView mWebView;
+
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         // The last two arguments ensure LayoutParams are inflated
         // properly.
-        View rootView = inflater.inflate(R.layout.webviewfragment, container, false);
+        mBaseView = inflater.inflate(R.layout.webviewfragment, container, false);
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        return rootView;
+        mWebView = (WebView) mBaseView.findViewById(R.id.web_view);
+        String uri = "http://groupedirectouest.com/";
+        mWebView.setWebViewClient(new MyWebViewClient());
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl(uri);
+        return mBaseView;
+    }
+
+    class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url); // Stay within this webview and load url
+                return true;
+        }
     }
 
 }
